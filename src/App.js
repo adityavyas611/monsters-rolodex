77642949472592650748a9ccb875aaeda7fefc2a
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-21 18:34:56 +0530

    Project Complete: Monsters Rolodex

--- src/App.js
+++ src/App.js
@@ -16,13 +16,19 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
-  }                                                                                               
+  } 
+  
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+  
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster => monster.name.toLowerCase().includes(searchField.toLowerCase())));
     return (
       <div className="App">
-        <SearchBox placeholder="search monsters" handleChange={e => this.setState({ searchField: e.target.value })}/>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="search monsters" handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters} />
       </div>
     );
