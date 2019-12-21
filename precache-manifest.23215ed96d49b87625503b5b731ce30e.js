577afdc1934d1e81c3a2e9484e268f4ccd2addbc
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-21 18:40:41 +0530

    Updates

---
+++ precache-manifest.23215ed96d49b87625503b5b731ce30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed181ad8f232ecc2915baad2e5697e6c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d4fb801baacd6dc03b2b",
+    "url": "/monsters-rolodex/static/css/main.cd4be674.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4fb801baacd6dc03b2b",
+    "url": "/monsters-rolodex/static/js/main.05cba492.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
