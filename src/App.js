fea1c63bea39e0f35a24078a003a462b332a0f82
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-21 18:05:33 +0530

    Added SearchBox Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import { CardList } from './components/card-list/card-list.component';
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.components";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -14,11 +16,14 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
-  }
+  }                                                                                               
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster => monster.name.toLowerCase().includes(searchField.toLowerCase())));
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <SearchBox placeholder="search monsters" handleChange={e => this.setState({ searchField: e.target.value })}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
